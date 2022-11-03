a1e33f7c48518fd9f1c6c57bcc3b27e4710a21c1
Kendall Carmona <kcarmona@rdscr.com>
2022-11-03 15:49:39 -0600

    validación del dominio

--- fe/src/app/main/maintenance/components/domains/domains.component.ts
+++ fe/src/app/main/maintenance/components/domains/domains.component.ts
@@ -62,6 +62,28 @@ export class DomainsComponent {
    * save and update the domains
    */
   saveDomain() {
+    const regex =
+      /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?|^((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gm;
+
+      if (!regex.test(this.domainForm.controls.domain.value)) {
+        return this.msg.add({
+          severity: MessagesTst.WARNING,
+          summary: 'El link del domino no es válido.',
+        });
+      }
+    if (!regex.test(this.domainForm.controls.matomoUrl.value)) {
+      return this.msg.add({
+        severity: MessagesTst.WARNING,
+        summary: 'El link de matomo no es válido.',
+      });
+    }
+    if (!regex.test(this.domainForm.controls.cromaUrl.value)) {
+      return this.msg.add({
+        severity: MessagesTst.WARNING,
+        summary: 'El link de croma no es válido.',
+      });
+    }
+
     if (this.domainForm.value._id) {
       // update widget
       this.domainService.update(this.domainForm.value).subscribe({
